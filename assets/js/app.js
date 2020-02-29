ffb0cdb478a16adcf3c56104b8f7fe5b39d4be65
Hickacou <leflon.paul@gmail.com>
2020-02-29 17:37:16 +0100

    Création de la section de paramètres

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(".parameter-line#n input").keydown(function () { displayCardOmega($(this), false) });
+    $(".parameter-line#n input").change(function () { displayCardOmega($(this), true) });
+});
+
+function displayCardOmega($this, correct) {
+    setTimeout(() => {
+        var n = $this.val();
+        var e = $("#cardOmega");
+        if (n > 0) e.text(scientificWriting(Math.pow(2, n)));
+        else {
+            e.text("∞")
+            if (correct) $this.val(0);
+        }
+    });
+}
+
+function scientificWriting(n) {
+    if (!n.toString().includes("e")) return n;
+    n = n.toString().split("e");
+    return parseFloat(n[0]).toFixed(3).toString().replace(".", ",") + ".10^" + parseInt(n[1]);
+}
