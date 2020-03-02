8cce2f05617a13de64af9a886e7d07dd06c90c99
Hickacou <leflon.paul@gmail.com>
2020-03-02 14:28:25 +0100

    Dernier commit avant le recommencement.

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,11 +1,19 @@
+var Settings = {
+    n: 0,
+    delta: 50,
+};
+
 $(function () {
-    $(".parameter-line#n input").keydown(function () { displayCardOmega($(this), false) });
-    $(".parameter-line#n input").change(function () { displayCardOmega($(this), true) });
+    $(".parameter-line#n input").keydown(function () { handleNChange($(this), false) });
+    $(".parameter-line#n input").change(function () { handleNChange($(this), true) });
+    $(".parameter-line#delta input").keydown(function () { handleDeltaChange($(this), false); });
+    $(".parameter-line#delta input").change(function () { handleDeltaChange($(this), true); });
 });
 
-function displayCardOmega($this, correct) {
+function handleNChange($this, correct) {
     setTimeout(() => {
-        var n = $this.val();
+        var n = parseInt($this.val());
+        Settings.n = n;
         var e = $("#cardOmega");
         if (n > 0) e.html((Math.pow(2, n) == Infinity)
             ? "2<sup>" + n + "</sup>"
@@ -14,6 +22,16 @@ function displayCardOmega($this, correct) {
         else {
             e.text("∞")
             if (correct) $this.val(0);
+        }
+    });
+}
+
+function handleDeltaChange($this, correct) {
+    setTimeout(function () {
+        var delta = parseInt($this.val());
+        Settings.delta = delta;
+        if (delta < 0 && correct) {
+            $this.val(0);
         }
     });
 }
