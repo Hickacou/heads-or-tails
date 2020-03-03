a1a61279030ab7e3c795e706dcef1431713e62fd
Hickacou <leflon.paul@gmail.com>
2020-03-03 14:34:12 +0100

    Création de la simulation, le tout en bordel.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,33 @@ const app = new Vue({
             interval: 50,
             tailsColor: "#FF0000",
             headsColor: "#0000FF"
-        }
+        },
+        simulationData: {
+            thrownCoins: 0,
+            historyString: "",
+            heads: {
+                count: 0,
+                streak: 0,
+                hightestStreak: 0,
+                percentage: 0,
+            },
+            tails: {
+                count: 0,
+                streak: 0,
+                hightestStreak: 0,
+                percentage: 0,
+            },
+            fiftyFifty: 0
+        },
+        section: "settings"
     },
     computed: {
         outcomes: function () {
             const n = this.simulationSettings.throws;
-            if (n == 0) return "∞";
-            if (n < 0) return this.simulationSettings.throws = 0;
+            if (n <= 0) {
+                this.simulationSettings.throws = 0
+                return "∞";
+            }
             var pow = Math.pow(2, n);
             if (pow == Infinity) return `2<sup>${n}</sup>`;
             pow = pow.toString();
@@ -24,11 +44,57 @@ const app = new Vue({
             }
             return (pow / Math.pow(10, pow.length - 1)).toFixed(3).toString().replace(".", ",") + ".10<sup>" + (pow.length - 1) + "</sup>";
         },
-        minDuration: function() {
+        minDuration: function () {
             const n = this.simulationSettings.throws;
             const d = this.simulationSettings.interval;
             if (n == 0) return "∞";
-            return n*d/1000 + "s";
+            return n * d / 1000 + "s";
+        },
+        simulationTimer: function () {
+            return "lol" + new Date();
+        }
+    },
+    methods: {
+        initSimulation: function () {
+            this.section = "simulation";
+            this.startedAt = new Date();
+            this.simulationInterval = setInterval(this.simulationIntervalFunc, this.simulationSettings.interval);
+        },
+        simulationIntervalFunc: function () {
+            const coin = throwCoin();
+            this.simulationData.thrownCoins++;
+            this.simulationData.historyString += (coin == "heads") ? "F" : "P";
+            this.simulationData[coin].count++;
+            this.simulationData[(coin == "heads") ? "tails" : "heads"].streak = 0;
+            if (++this.simulationData[coin].streak > this.simulationData[coin].highestStreak) {
+                this.simulationData[coin].highestStreak = this.simulationData[coin].streak;
+            }
+            console.log(this.simulationData[coin].count + "/" + this.simulationData.thrownCoins);
+            this.simulationData[(coin == "heads") ? "tails" : "heads"].percentage = (this.simulationData[(coin == "heads") ? "tails" : "heads"].count / this.simulationData.thrownCoins) * 100;
+            this.simulationData[(coin == "heads") ? "heads" : "tails"].percentage = (this.simulationData[(coin == "heads") ? "heads" : "tails"].count / this.simulationData.thrownCoins) * 100;
+            if (this.simulationData["heads"].count == this.simulationData["tails"].count) {
+                this.simulationData.fiftyFifty++;
+            }
+            //Je dois vite boucler et j'ai pas réussi à régler les heights avec v-bind.
+            this.$el.querySelector("#simulation-textarea").scrollTop = this.$el.querySelector("#simulation-textarea").scrollHeight; 
+            this.$el.querySelector("#simulation-histogram .tails").style.height = this.simulationData.tails.percentage + "%";
+            this.$el.querySelector("#simulation-histogram .tails").style.background = this.simulationSettings.tailsColor;
+            this.$el.querySelector("#simulation-histogram .heads").style.height = this.simulationData.heads.percentage + "%";
+            this.$el.querySelector("#simulation-histogram .heads").style.background = this.simulationSettings.headsColor;
+            if (this.simulationData.thrownCoins == this.simulationSettings.throws) {
+                this.stopSimulation();
+            }
+        },
+        stopSimulation: function() {
+            clearInterval(this.simulationInterval);
         }
     }
 });
+
+
+function throwCoin() {
+    const r = Math.random();
+    if (r > 0.5) return "heads";
+    if (r < 0.5) return "tails";
+    return throwCoin();
+}
--- assets/js/components.js
+++ assets/js/components.js
@@ -1,4 +1,14 @@
 Vue.component("app-section", {
     props: ["title"],
-    template: "<div class='app-section'><div class='app-section-title'> {{ title }}</div><slot></slot></div>"
+    template: "<div class='app-section'><div class='app-section-title'>{{ title }}</div><slot></slot></div>"
+});
+
+Vue.component("histogram", {
+    props: ["data"],
+    template: `<div class='histogram'>
+    <div class="tails">
+        <b>Piles : {{ data.simulationData.tails.percentage.toFixed(2) }}%</b></div>
+    <div class="heads">
+        <b>Faces : {{ data.simulationData.heads.percentage.toFixed(2) }}%</b></div>
+    </div>`
 });
