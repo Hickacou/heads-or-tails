c96157ce442dd57d833f95bbeb83a22c51487720
Hickacou <leflon.paul@gmail.com>
2020-03-03 09:11:00 +0100

    Création de la section paramètres et des premiers designs.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,16 @@ const app = new Vue({
     data: {
         simulationSettings: {
             throws: 0,
+            interval: 50,
+            tailsColor: "#FF0000",
+            headsColor: "#0000FF"
         }
     },
     computed: {
         outcomes: function () {
             const n = this.simulationSettings.throws;
             if (n == 0) return "∞";
+            if (n < 0) return this.simulationSettings.throws = 0;
             var pow = Math.pow(2, n);
             if (pow == Infinity) return `2<sup>${n}</sup>`;
             pow = pow.toString();
@@ -19,6 +23,12 @@ const app = new Vue({
                 return pow[0].substring(0, 5).replace(".", ",") + ".10<sup>" + pow[1].slice(1) + "</sup>";
             }
             return (pow / Math.pow(10, pow.length - 1)).toFixed(3).toString().replace(".", ",") + ".10<sup>" + (pow.length - 1) + "</sup>";
+        },
+        minDuration: function() {
+            const n = this.simulationSettings.throws;
+            const d = this.simulationSettings.interval;
+            if (n == 0) return "∞";
+            return n*d/1000 + "s";
         }
     }
 });
